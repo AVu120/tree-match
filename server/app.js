358f8060d5b8f3515444585b4ebe9b290af2816b
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-04-04 13:12:22 +1000

    Remove redundant word in api routes.

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-app.use("/api", rootRouter);
+app.use("/", rootRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
